a2841804f04c631f66c5e4c30e6bdc7b4ada354a
cpuccino <thegreatcappuccino@gmail.com>
2021-09-03 20:28:47 +1000

    feat: Added disk partition name

--- src/services/sysinfo.ts
+++ src/services/sysinfo.ts
@@ -4,15 +4,15 @@ export function getSysInfo() {
     const delimeter = '#!$'
 
     const cpuInfoJson = execSync(`mpstat -P ALL -o JSON -u`).toString();
-    const diskUsageRaw = execSync(`df -hT | awk '{print $2 "${delimeter}" $3 "${delimeter}" $4 "${delimeter}" $5}'`).toString();
+    const diskUsageRaw = execSync(`df -hT | awk '{print $1 "${delimeter}" $2 "${delimeter}" $3 "${delimeter}" $4 "${delimeter}" $5}'`).toString();
     const memoryUsageRaw = execSync(`free -m | awk '/^Mem/ {print $2 "${delimeter}" $3 "${delimeter}" $4}'`).toString();
 
     const cpuInfo = JSON.parse(cpuInfoJson);
     const { release, machine, statistics } = cpuInfo?.sysstat?.hosts[0];
     const cpuLoad = statistics[0]['cpu-load'].map((cpu: any) => ({ name: cpu.cpu, usage: cpu.usr, idle: cpu.idle }));
     const diskUsage = diskUsageRaw.split('\n').slice(1, -1).map(entry => {
-        const [type, size, used, available] = entry.split(delimeter);
-        return { type, size, used, available };
+        const [partition, type, size, used, available] = entry.split(delimeter);
+        return { partition, type, size, used, available };
     });
     const [total, used, free] = memoryUsageRaw.split(delimeter).map(info => info.trim());
     const memoryUsage = { total, used, free };
